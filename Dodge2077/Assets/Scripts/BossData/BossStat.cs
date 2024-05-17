fd9ee57a7081730d6896de699ab593aaa0494a45
poi001 <107660181+poi001@users.noreply.github.com>
2024-05-17 20:40:27 +0900

    +BossHPBar

---
+++ Dodge2077/Assets/Scripts/BossData/BossStat.cs
@@ -0,0 +1,16 @@
+//[System.Serializable]
+
+//public class BossStat : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
