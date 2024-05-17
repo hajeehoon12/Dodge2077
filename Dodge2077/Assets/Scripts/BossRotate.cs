db49afd7419315fac659b077af455539cd08521f
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-05-17 16:54:38 +0900

    +Boss Appear Animation

--- Dodge2077/Assets/Scripts/BossRotate.cs
+++ Dodge2077/Assets/Scripts/BossRotate.cs
@@ -8,16 +8,38 @@ public class BossRotate : MonoBehaviour
     public GameObject LightBoss;
     public GameObject DarkBoss;
 
+    private bool isRotate = false; 
+
     // Update is called once per frame
 
     public void Start()
     {
-        RotateBoss();
+        transform.DOMove(new Vector3(0, 2.5f, 0), 3f);
+        Invoke("StartRotate", 5f);
+    }
+
+    private void StartRotate()
+    {
+        StartCoroutine(RotateBoss(5f));
     }
 
 
-    public void RotateBoss()
+    private IEnumerator RotateBoss(float RotateTime)
     {
-        transform.DORotate(new Vector3 (0, 0, 180), 1);
+        while (true)
+        {
+            if (!isRotate)
+            {
+                transform.DORotate(new Vector3(0, 0, 180), 1);
+                isRotate = true;
+                yield return new WaitForSeconds(RotateTime);
+            }
+            else
+            {
+                transform.DORotate(new Vector3(0, 0, 360), 1);
+                isRotate = false;
+                yield return new WaitForSeconds(RotateTime);
+            }
+        }
     }
 }
