8b946bc1644c1501c2d3d74f11a25724ca5a7a14
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2024-05-17 16:55:42 +0900

    Revert "+Boss Appear Animation"
    This reverts commit db49afd7419315fac659b077af455539cd08521f.

--- Dodge2077/Assets/Scripts/BossRotate.cs
+++ Dodge2077/Assets/Scripts/BossRotate.cs
@@ -8,38 +8,16 @@ public class BossRotate : MonoBehaviour
     public GameObject LightBoss;
     public GameObject DarkBoss;
 
-    private bool isRotate = false; 
-
     // Update is called once per frame
 
     public void Start()
     {
-        transform.DOMove(new Vector3(0, 2.5f, 0), 3f);
-        Invoke("StartRotate", 5f);
-    }
-
-    private void StartRotate()
-    {
-        StartCoroutine(RotateBoss(5f));
+        RotateBoss();
     }
 
 
-    private IEnumerator RotateBoss(float RotateTime)
+    public void RotateBoss()
     {
-        while (true)
-        {
-            if (!isRotate)
-            {
-                transform.DORotate(new Vector3(0, 0, 180), 1);
-                isRotate = true;
-                yield return new WaitForSeconds(RotateTime);
-            }
-            else
-            {
-                transform.DORotate(new Vector3(0, 0, 360), 1);
-                isRotate = false;
-                yield return new WaitForSeconds(RotateTime);
-            }
-        }
+        transform.DORotate(new Vector3 (0, 0, 180), 1);
     }
 }
